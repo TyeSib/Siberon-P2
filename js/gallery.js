7fee03145b730578978bf76991308e75a95e452e
Tyler Siberon <122tsiberon@frhsd.com>
2021-11-12 08:12:50 -0500

    did the XMLHttpRequest off the video

--- js/gallery.js
+++ js/gallery.js
@@ -46,6 +46,16 @@ var mCurrentIndex = 0;
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
 
+if request. addEventListener('readystatechange', () => {
+//console.log(request, request.readyState);
+if(request.readyState === 4 && request.status === 200){
+console.log(request, request.responseText);
+} else if (request.readyState === 4) {
+console.log('could not fetch the data').
+});
+
+request.open ('GET','https://jsonplaceholder.typicode.com/todoss/');
+request.send();
 // Array holding GalleryImage objects (see below).
 var mImages = [];
 
