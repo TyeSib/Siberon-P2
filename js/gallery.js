d17d350406c5e7a9d6b6b8af19dcf17970bd36db
Tyler Siberon <122tsiberon@frhsd.com>
2021-11-15 10:40:50 -0500

    fixed the http

--- js/gallery.js
+++ js/gallery.js
@@ -49,7 +49,7 @@ var mRequest = new XMLHttpRequest();
 if request. addEventListener('readystatechange', () => {
 //console.log(request, request.readyState);
 if(request.readyState === 4 && request.status === 200){
-console.log(request, request.responseText);
+console.log(request,request.responseText);
 } else if (request.readyState === 4) {
 console.log('could not fetch the data').
 });
